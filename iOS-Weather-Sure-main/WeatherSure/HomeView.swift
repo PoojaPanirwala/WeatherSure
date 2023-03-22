7c1498cf7c40bf272dcbb09d7fa856cf617bc4be
Pooja Panirwala <poojapanirwala24@gmail.com>
2023-03-22 16:20:38 -0400

    saved locations tab is created in swiftUI

--- iOS-Weather-Sure-main/WeatherSure/HomeView.swift
+++ iOS-Weather-Sure-main/WeatherSure/HomeView.swift
@@ -15,21 +15,23 @@ struct HomeView: View {
             TabView {
                 // First tab content for Home
                 NavigationView {
+                    
                     ZStack {
                         Color(red: 0.902, green: 0.890, blue: 0.851)
                             .ignoresSafeArea(.all)
-                       
+                        Text("LOCATIONS")
+                        .foregroundColor(Color.brown)
+                        .font(.system(size: 26, weight: .bold))
+                        .offset(y: -UIScreen.main.bounds.height / 3)
                             List {
                                 
                                 // Show current location at the top
                                 HStack {
                                     
                                     Text("Current Location")
-                                        .bold()
                                     Spacer()
                                     Text("72°F")
                                 }
-                                
                                 .padding(.vertical)
                                 
                                 // Example location and temperature data
@@ -38,31 +40,35 @@ struct HomeView: View {
                                     Spacer()
                                     Text("60°F")
                                 }
+                                .padding(.vertical)
                                 HStack {
                                     Text("Los Angeles")
                                     Spacer()
                                     Text("70°F")
                                 }
+                                .padding(.vertical)
                                 HStack {
                                     Text("Chicago")
                                     Spacer()
                                     Text("55°F")
                                 }
+                                .padding(.vertical)
                             }
-                            
                             .listStyle(PlainListStyle())
                             .padding(.top, 50)
-                            .offset(y: UIScreen.main.bounds.height / 10)
+                            .foregroundColor(Color.brown)
+                            .font(.system(size: 22, weight: .medium))
+                            .offset(y: UIScreen.main.bounds.height / 5)
                         
                         
                         // Search bar
-                        TextField("Search", text: $search)
+                        TextField("Search here", text: $search)
                             .padding()
                             .background(Color.white)
                             .cornerRadius(30)
                             .padding(.horizontal)
                             .padding(.bottom, 10)
-                            .offset(y: -UIScreen.main.bounds.height / 3)
+                            .offset(y: -UIScreen.main.bounds.height / 4)
                     }
                     
                     //.navigationBarTitle(Text("Home"))
@@ -71,9 +77,46 @@ struct HomeView: View {
                     Image(systemName: "house")
                     Text("Home")
                 }
-                
-                // Second tab content for Saved Locations
-                Text("Saved Locations")
+                NavigationView {
+                    // Second tab content for Saved Locations
+                    ZStack {
+                        Color(red: 0.902, green: 0.890, blue: 0.851)
+                            .ignoresSafeArea(.all)
+                        Text("Saved LOCATIONS")
+                        .foregroundColor(Color.brown)
+                        .font(.system(size: 24, weight: .bold))
+                        .offset(y: -UIScreen.main.bounds.height / 3)
+//                        .offset(x:-UIScreen.main.bounds.width / 6)
+                        List {
+                            // Example location and temperature data
+                            HStack {
+                                Text("New York")
+                                Spacer()
+                                Text("60°F")
+                            }
+                            .padding(.vertical)
+                            HStack {
+                                Text("Los Angeles")
+                                Spacer()
+                                Text("70°F")
+                            }
+                            .padding(.vertical)
+                            HStack {
+                                Text("Chicago")
+                                Spacer()
+                                Text("55°F")
+                            }
+                            .padding(.vertical)
+                        }
+                        .foregroundColor(Color.brown)
+                        .font(.system(size: 22, weight: .medium))
+                        .listStyle(PlainListStyle())
+                        .padding(.top, 50)
+                        .offset(y: UIScreen.main.bounds.height / 10)
+                        
+                    }
+                }
+            
                     .tabItem {
                         Image(systemName: "location")
                         Text("Saved Locations")
@@ -89,6 +132,7 @@ struct HomeView: View {
             }
             .accentColor(Color.brown)
             .foregroundColor(Color.black)
+            .edgesIgnoringSafeArea(.top) // Ignore the safe area at the top
         }
     }
     struct HomeView_Previews: PreviewProvider {
